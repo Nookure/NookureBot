1090d13c9ba7b0a89a5918a196d7a03505359c4d
Angelillo15 <contact@angelillo15.es>
2024-02-25 23:31:29 +0000

    Create data script

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+import { MessageAnalysis, MessageAnalysisType } from './types';
+
+const data: MessageAnalysis[] = [
+  {
+    search: ['help nooktheme', 'nooktheme not work', 'nooktheme error'],
+    type: MessageAnalysisType.Contains,
+    reply: `
+  \`\`\`sh
+  cd /var/www/pterodactyl
+
+  php artisan down
+
+  curl -L https://github.com/Nookure/NookTheme/releases/latest/download/panel.tar.gz | tar -xzv
+
+  chmod -R 755 storage/* bootstrap/cache
+
+  composer install --no-dev --optimize-autoloader
+
+  php artisan view:clear
+  php artisan config:clear
+
+  php artisan migrate --seed --force
+
+  chown -R www-data:www-data /var/www/pterodactyl/* # NGINX/APACHE (Not CentOS, check GitHub for CentOS cmds)
+
+  php artisan queue:restart
+
+  php artisan up
+  \`\`\`
+  `,
+  },
+];
+
+export default data;

c5fd82eac458b14783f27a4fbc07945de6e319bc
Angelillo15 <contact@angelillo15.es>
2024-02-26 10:22:02 +0000

    Add interaction

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,7 +1,8 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 interface command {
   data: SlashCommandBuilder;
+  execute: (interaction: CommandInteraction) => void;
 }
 
 export default command;

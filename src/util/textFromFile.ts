8f758d0649f0cb78155e924aaab7600a4fdc2d1c
Hugo Torres <46359630+Reddishye@users.noreply.github.com>
2024-02-26 19:52:10 +0000

    Create readFile auxiliar function

---
+++ src/util/textFromFile.ts
@@ -0,0 +1,21 @@
+import https from 'https';
+import { Attachment } from 'discord.js';
+
+const readFile = async (attachment: Attachment): Promise<string> => {
+    return new Promise((resolve, reject) => {
+         const url = attachment.url;
+            https.get(url, (response) => {
+                let data = '';
+                response.on('data', (chunk) => {
+                    data += chunk;
+                });
+                response.on('end', () => {
+                    resolve(data);
+                });
+            }).on('error', (error) => {
+                reject(error);
+            });
+    } );
+}
+
+export default readFile;

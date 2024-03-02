7bbc86e7e7e255cf377ffb719c9eb6317602bed5
Hugo Torres <46359630+Reddishye@users.noreply.github.com>
2024-03-02 17:12:31 +0000

    Add id, description and emoji fields for dropdown

--- src/config/tickets.ts
+++ src/config/tickets.ts
@@ -1,25 +1,40 @@
 export interface category {
+  id: string;
   name: string;
+  description?: string;
   channelPrefix: string;
+  emoji?: string;
 }
 
 export default {
   categories: [
     {
+      id: 'pterodactyl',
       name: 'Pterodactyl',
+      description: 'Pterodactyl related tickets',
       channelPrefix: '🐦-Pterodactyl-{username}',
+      emoji: '🐦',
     },
     {
+      id: 'nookurestaff',
       name: 'NookureStaff',
+      description: 'Nookure Staff related tickets',
       channelPrefix: '🔧-NookureStaff-{username}',
+      emoji: '🔧',
     },
     {
+      id: 'nookurechat',
       name: 'NookureChat',
+      description: 'Nookure Chat related tickets',
       channelPrefix: '🗣️-NookureChat-{username}',
+      emoji: '🗣️',
     },
     {
+      id: 'comissions',
       name: 'Nookure Comissions',
+      description: 'Nookure Comissions related tickets',
       channelPrefix: '🌿-Comissions-{username}',
+      emoji: '🌿',
     },
   ] as category[],
 };

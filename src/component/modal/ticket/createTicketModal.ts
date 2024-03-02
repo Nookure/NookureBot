1bb8f3e534aed1fd505916deae5cdd3856a41d5a
Hugo Torres <46359630+Reddishye@users.noreply.github.com>
2024-03-02 17:11:01 +0000

    Get selected category

--- src/component/modal/ticket/createTicketModal.ts
+++ src/component/modal/ticket/createTicketModal.ts
@@ -1,5 +1,6 @@
 import { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, ModalSubmitInteraction } from 'discord.js';
 import { modalHandlerRegistry } from '../modalHandler';
+import userSelections from '@/util/userSelections';
 export const customId = 'create-ticket-modal';
 
 export const createTicketModal = new ModalBuilder()
@@ -11,7 +12,7 @@ export const createTicketModal = new ModalBuilder()
         .setCustomId('ticket-title')
         .setLabel('Title')
         .setStyle(TextInputStyle.Short)
-        .setPlaceholder('Enter the title of your ticket')
+        .setPlaceholder('Error when...')
         .setRequired(true)
     ),
     new ActionRowBuilder<TextInputBuilder>().addComponents(
@@ -19,15 +20,17 @@ export const createTicketModal = new ModalBuilder()
         .setCustomId('ticket-description')
         .setLabel('Description')
         .setStyle(TextInputStyle.Paragraph)
-        .setPlaceholder('Enter the description of your issue')
+        .setPlaceholder('Greetings, I am having trouble with...')
         .setRequired(true)
-    )
+    ),
   );
 
 export async function handleCreateTicketModalSubmit(interaction: ModalSubmitInteraction) {
+  const userSelectedValue = userSelections.get(interaction.user.id);
   const title = interaction.fields.getTextInputValue('ticket-title');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const description = interaction.fields.getTextInputValue('ticket-description');
-  await interaction.reply({ content: `Ticket created with title: ${title}`, ephemeral: true });
+  await interaction.reply({ content: `Ticket created with title: ${title}, ${ userSelectedValue }`, ephemeral: true });
+  userSelections.delete(interaction.user.id);
 }
 modalHandlerRegistry.registerModal(customId, { handleModalSubmit: handleCreateTicketModalSubmit });

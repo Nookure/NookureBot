022cb6224cf21a02fd1d3a6968a5dc2c41d2ad4b
Hugo Torres <46359630+Reddishye@users.noreply.github.com>
2024-03-04 21:57:06 +0000

    Add suggestions language settings

--- src/lang.ts
+++ src/lang.ts
@@ -15,4 +15,57 @@ export default {
       style: ButtonStyle.Primary,
     },
   },
+  suggestions: {
+    title: 'Suggestions',
+    description: `
+      > If you have any suggestions click the button below!
+      > Our staff team will be shortly with you!
+    `,
+    color: 'Green' as ColorResolvable,
+    thumbnail: 'https://avatars.githubusercontent.com/u/115893913?s=280&v=4',
+    footer: 'Nookure Studios © ' + new Date().getFullYear(),
+    createSuggestionButton: {
+      label: '📩 Create Suggestion',
+      style: ButtonStyle.Primary,
+    },
+    createSuggestionModal: {
+      title: 'Create Suggestion',
+      description: 'Create a suggestion for the server',
+      fields: {
+        product: {
+          id: 'product',
+          label: 'Product',
+          placeholder: 'Nookure Staff',
+          default: '',
+        },
+        suggestion: {
+          id: 'suggestion',
+          label: 'Suggestion',
+          placeholder: 'I would implement...',
+          default: '',
+        },
+      },
+      submitReply: 'Suggestion created!',
+    },
+    // Available placeholders: {user}, {user.displayName}, {user.mention}, {user.avatarUrl}, {user.tag}, {user.id}, {field.<name>}.
+    suggestionEmbed: {
+      title: 'New Suggestion from {user.displayName}',
+      description: '',
+      color: 'Green' as ColorResolvable,
+      thumbnail: '{user.avatarUrl}',
+      footer: 'Nookure Studios © ' + new Date().getFullYear(),
+      fields: {
+        product: {
+          name: '**Product:**',
+          value: '```{field.product}```',
+          inline: false,
+        },
+        suggestion: {
+          name: '**Suggestion:**',
+          value: '```{field.suggestion}```',
+          inline: false,
+        },
+      },
+    },
+  },
 };
